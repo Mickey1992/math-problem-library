661dd0f0609bb50bae657d9b52c4c964a93be117
Mickey1992 <wl920425@gmail.com>
2024-04-02 23:25:06 +0800

    navigate to top-page when clicking icon on the header part

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/profile-photo.webp";
 
 export default function Header() {
+	//const navigate = useNavigate();
 	return (
 		<>
 			<div id="header">
-				<img src={logo} alt="a girl is solving math problem" />
+				<Link to="/">
+					<img src={logo} alt="a girl is solving math problem" />
+				</Link>
 			</div>
 			<Outlet />
 		</>
