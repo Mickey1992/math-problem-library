f2c19dca30f8ad0d6c87937ff25e0b7b9bc59dda
Mickey1992 <wl920425@gmail.com>
2024-04-02 19:48:28 +0800

    add state to save image's position

--- src/components/DraggableImg.tsx
+++ src/components/DraggableImg.tsx
@@ -1,15 +1,33 @@
-import { motion } from "framer-motion";
+import { motion, PanInfo } from "framer-motion";
+
+import { useState } from "react";
 
 export default function DraggableImg({ imgUrl }) {
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+	console.log(position);
+
+	function onDragEnd(
+		event: MouseEvent | TouchEvent | PointerEvent,
+		info: PanInfo
+	) {
+		setPosition((prev) => ({
+			x: prev.x + info.offset.x,
+			y: prev.y + info.offset.y,
+		}));
+	}
+
 	return (
 		<motion.img
 			src={imgUrl}
 			key={imgUrl}
+			initial={false}
+			animate={position}
 			drag
 			whileDrag={{
 				border: "1px dotted blue",
 				"border-radius": "5px",
 			}}
+			onDragEnd={onDragEnd}
 		/>
 	);
 }
