b3e223a7b303f1c9cef8cb318046f1f831de875b
Mickey1992 <wl920425@gmail.com>
2024-04-03 11:10:30 +0800

    add navigation-bar on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import logo from "../assets/profile-photo.webp";
+
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 
 export default function Header() {
 	//const navigate = useNavigate();
@@ -9,6 +12,34 @@ export default function Header() {
 				<Link to="/">
 					<img src={logo} alt="a girl is solving math problem" />
 				</Link>
+
+				<menu>
+					<li>
+						<NavLink
+							to="/"
+							className={({
+								isActive,
+								isPending,
+								isTransitioning,
+							}) =>
+								[
+									isPending ? "pending" : "",
+									isActive ? "active" : "",
+									isTransitioning ? "transitioning" : "",
+								].join(" ")
+							}
+						>
+							<LibraryBooksOutlinedIcon />
+							Problems
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/add">
+							<AddOutlinedIcon />
+							Add
+						</NavLink>
+					</li>
+				</menu>
 			</div>
 			<Outlet />
 		</>
